2ecaa81a6287252f08f396e666167c1d618a4244
Kaspars Ciganovics <kasparsciganovics@Kasparss-iMac-Pro.local>
2021-01-23 23:56:56 +0200

    new xcode project created

---
+++ exemplesFromBook/exemplesFromBook/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  exemplesFromBook
+//
+//  Created by Kaspars Ciganovics on 23/01/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
