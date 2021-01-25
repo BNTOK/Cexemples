83567e346bebf8f342eba13442247dccff52a771
Kaspars <kasparscig@gmail.com>
2021-01-25 19:48:07 +0000

    second function added

--- functions.c
+++ functions.c
@@ -9,13 +9,24 @@ void hello(void)
 
 void exercise21(void)
 {
-    int a;
     /* Write program to determine the ranges of char, short, int
     and long variables, both signed and unsigned, by printing approriate values
     from standart headers and by durect computation. Harder if you compute
-    them: determine the ranges of the varios floating-point types*/
-    printf("%d\n", INT_MAX);
+    them: determine the ranges of the varios floating-point types
+    %f print floating poin %6.2f at least 6 wide and 2 after decimal*/
+
+    char a, b ,c;
+    a = b = 80;
+    c = a + b;
+    printf("%f\n", c);
     printf("%d\n", INT_MIN);
     printf("%d\n", CHAR_BIT);
-    printf("%d\n", &a);
+    printf("%f\n", FLT_ROUNDS);
+    printf("%f\n", __FLT_RADIX__);
+    printf("%f\n", FLT_MANT_DIG);
+    printf("%f\n", FLT_DIG);
+    printf("%f\n", FLT_MIN_EXP);
+    printf("%f\n", FLT_MAX_EXP);
+    printf("%f\n", __FLT_MIN_10_EXP__);
+    printf("%f\n", __FLT_MAX_10_EXP__);
 }
